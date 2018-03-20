95f2807ccb1f0efdff58ad44f7b76720d41b0134
Joel Clair <clair.joel@yahoo.com>
2018-03-20 10:38:55 -0700

    nav bar added

---
+++ src/components/landing-page/landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Landing extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Water Loss Tracker</h1>
+        <p>checkout the dashboard page to create a form</p>
+        <p>checkout the reports page to view your forms</p>
+      </div>
+    );
+
+  }
+}
+
+export default Landing;
