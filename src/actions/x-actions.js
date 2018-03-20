4f51e5aa1604b951ad087e24161a04dfffd88c8b
Mitchell M <mitchellm1618@gmail.com>
2018-03-20 14:10:29 -0700

    saving to test full front-back database

--- src/actions/x-actions.js
+++ src/actions/x-actions.js
@@ -1,4 +1,10 @@
 import uuid from 'uuid/v4';
+const __API_URL__ = process.env.API_URL;
+
+export const xGet = xs => ({
+  type: 'X_GET',
+  payload: xs,
+});
 
 export const xCreate = x => {
   x.id = uuid();
@@ -20,3 +26,18 @@ export const xDelete = x => ({
 });
 
 export const xReset = () => ({type: 'X_RESET'});
+
+
+export const xFetchRequest = () => dispatch => {
+  return superagent.get(`${__API_URL__}/customer`)
+    .then(res => dispatch(xGet(res.body)))
+    .catch(logError);
+};
+
+export const xCreateRequest = x => (dispatch, getState) => {
+  console.log('apiurl:', __API_URL__);
+  return superagent.post(`${__API_URL__}/customer`)
+    .send(x)
+    .then(res => dispatch(xCreate(res.body)))
+    .catch(logError);
+};
