036098b505f2a08f720145b2d84ccb1b693048b0
Mitchell M <mitchellm1618@gmail.com>
2018-03-22 19:47:21 -0700

    saving work

--- src/__test__/dashboard.test.js
+++ src/__test__/dashboard.test.js
@@ -12,9 +12,9 @@ describe('<Dashboard />', function() {
   describe('#full mounting', function() {
     beforeAll(() => {
       let wrapper = mount(<Provider store={createStore()}><Dashboard /></Provider>);
-      wrapper.setProps({categories: [
-        {id: '1234', title: 'yooo', timestamp: new Date()},
-        {id: '5678', title: 'gruu', timestamp: new Date()}]});
+      wrapper.setProps({customer: [
+        {_id: '1234', name: 'yooo'},
+        {_id: '5678', name: 'gruu'}]});
       this.wrapper = wrapper;
     });
     afterAll(() => this.wrapper.unmount());
