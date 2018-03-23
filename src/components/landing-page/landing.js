e7c802c5df2a66444fe4d658f39eeec6cd457bdb
Joel Clair <clair.joel@yahoo.com>
2018-03-23 10:03:03 -0700

    css update

--- src/components/landing-page/landing.js
+++ src/components/landing-page/landing.js
@@ -6,10 +6,21 @@ class Landing extends React.Component{
       <div id="landing">
         <h1>Water Loss Reporter</h1>
 
-        <img src={require('../../styles/assets/logo.png')} height="250" width="250"/>
-        <p>checkout the Dashboard page to create forms</p>
-        <p>checkout the Reports page to review your forms</p>
-        <p>checkout the Statistics page to view your form data</p>
+        <img src={require(`${__dirname}/logo.png`)} height="250" width="250"/>
+        <div className="landing-wrapper">
+          <div className="landing-report">
+            <h1>View Forms</h1>
+            <a href="reports"><img src={require(`${__dirname}/openbook.png`)} height="250" width="250"/></a>
+          </div>
+          <div className="landing-dash">
+            <h1>Create Forms</h1>
+            <a href="dashboard"><img src={require(`${__dirname}/penandpaper.png`)} height="250" width="250"/></a>
+          </div>
+          <div className="landing-stats">
+            <h1>View Statistics</h1>
+            <a href="chart"><img src={require(`${__dirname}/stats.png`)} height="250" width="250"/></a>
+          </div>
+        </div>
 
       </div>
     );
