e5e8264b9ed732fe9e3bcf97a63fd9c4365cbdb5
Mitchell M <mitchellm1618@gmail.com>
2018-03-22 19:29:07 -0700

    reducer tests passing

--- src/reducers/report.js
+++ src/reducers/report.js
@@ -4,10 +4,10 @@ export default (state=initialState, action) => {
   let {type, payload} = action;
 
   switch(type) {
-  case 'CUSTOMER_CREATE': return {...state, [payload.id]: []};
+  case 'CUSTOMER_CREATE': return {...state, [payload._id]: []};
   case 'CUSTOMER_DELETE':
     let changedState = {...state};
-    delete changedState[payload.id];
+    delete changedState[payload._id];
     return changedState;
 
   case 'REPORT_ID_CREATE': return [...state, payload];
@@ -19,13 +19,13 @@ export default (state=initialState, action) => {
   case 'REPORT_GET': return payload;
   case 'REPORT_UPDATE':
     // if(payload.name === '' || payload.cost == '') return new Error('submit a name and cost');
-    let customerStateUpdate = state[payload.customerId].map(
-      report => report.id === payload.id ? payload : report);
-    return {...state, [payload.customerId]: customerStateUpdate};
+    let customerStateUpdate = state[payload.customer].map(
+      report => report._id === payload._id ? payload : report);
+    return {...state, [payload.customer]: customerStateUpdate};
   case 'REPORT_DELETE':
-    let customerStateDelete = state[payload.customerId].filter(
-      report => report.id !== payload.id);
-    return {...state, [payload.customerId]: customerStateDelete};
+    let customerStateDelete = state[payload.customer].filter(
+      report => report._id !== payload._id);
+    return {...state, [payload.customer]: customerStateDelete};
   case 'REPORT_RESET': return initialState;
   default: return state;
   }
